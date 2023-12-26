a910e14c01fe0ca5557ee2826d418e3dfbe054c3
Federico Deniard <fededeniard@gmail.com>
2023-12-26 18:51:58 -0300

    fixed .js path

--- assets/components/header/header.js
+++ assets/components/header/header.js
@@ -4,7 +4,7 @@ class copyrioHeader extends HTMLElement{
     }
     connectedCallback(){
         this.innerHTML = 
-    `<link rel="stylesheet" type="text/css" href="../..//assets/components/header/header.css">
+    `<link rel="stylesheet" type="text/css" href="../../assets/components/header/header.css">
     <header>
         <h1><a href="./index.html">COPYRIO</a></h1>
     </header>`
