886f4f3c685db36bf8c51982b5f91b0ee2dcccd2
Federico Deniard <fededeniard@gmail.com>
2023-12-26 17:06:58 -0300

    route changed

--- assets/components/footer/footer.js
+++ assets/components/footer/footer.js
@@ -4,14 +4,14 @@ class copyrioFooter extends HTMLElement{
     }
     connectedCallback(){
         this.innerHTML = 
-    `<link rel="stylesheet" type="text/css" href="/assets/components/footer/footer.css">
+    `<link rel="stylesheet" type="text/css" href="../../assets/components/footer/footer.css">
     <footer>
         <div class="location">
-            <img src="/assets/components/footer/location.svg">
+            <img src="../..//assets/components/footer/location.svg">
             <p> Zona Balvanera. Av. Corrientes 2330 Piso 9° - Oficina 904</p>
         </div>
         <div class="contact">
-         <img src="/assets/components/footer/phone.svg">
+         <img src="../..//assets/components/footer/phone.svg">
          <p>+(54)(11)xxxx-xxxx</p>
     </footer>`
     }
