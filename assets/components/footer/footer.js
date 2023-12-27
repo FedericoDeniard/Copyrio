98516686d12ddd54b522ccec2ad01f9892a6c656
Federico Deniard <fededeniard@gmail.com>
2023-12-27 20:55:47 -0300

    .

--- assets/components/footer/footer.js
+++ assets/components/footer/footer.js
@@ -11,8 +11,14 @@ class copyrioFooter extends HTMLElement{
             <p> Zona Balvanera. Av. Corrientes 2330 Piso 9° - Oficina 904</p>
         </a>
         <div class="contact">
-                <img src="../../assets/components/footer/phone.svg">
-                <p>+(54)(11)xxxx-xxxx</p>
+            <img src="../../assets/components/footer/phone.svg">
+            <p>+(54)(11)4951-6582</br>
+               +(54)(11)4954-3444</p>
+        </div>
+        <div class="time">
+            <img src="../../assets/components/footer/time.svg">
+            <p>Lunes a Viernes de 10:30 a 19:00 Hs.</p>
+        </div>
     </footer>`
     }
 }
